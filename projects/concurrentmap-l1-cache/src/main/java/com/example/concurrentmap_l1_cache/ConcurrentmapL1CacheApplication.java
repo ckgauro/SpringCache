d996702aabdadd25855b043c585b66ad12727f81
ckgauro <ckgauro@gmail.com>
2025-06-04 11:38:50 -0400

    Spring Cache

---
+++ projects/concurrentmap-l1-cache/src/main/java/com/example/concurrentmap_l1_cache/ConcurrentmapL1CacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurrentmap_l1_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentmapL1CacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentmapL1CacheApplication.class, args);
+	}
+
+}
---
+++ projects/concurrentmap-l1-cache/src/test/java/com/example/concurrentmap_l1_cache/ConcurrentmapL1CacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concurrentmap_l1_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentmapL1CacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
